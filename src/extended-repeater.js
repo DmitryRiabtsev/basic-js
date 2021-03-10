0a3f54ccbadcd33be00966b055177758155562b3
Dmitry <dim4ik7558118@gmail.com>
2021-03-10 20:37:32 +0300

    fix: ready test

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,35 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  if(options.repeatTimes == undefined && options.additionRepeatTimes == undefined){
+    return str + options.addition ;
+  } else {
+    let arrayString =[];
+  let arrayAddition = [];
+   for(let i = 1; i <= options.repeatTimes; i++){
+     arrayString.push(str)
+   }
+
+   for(let j = 1; j <= options.additionRepeatTimes; j++){
+     if(options.addition == null){
+      arrayAddition.push('null')
+     }else{
+      arrayAddition.push(options.addition)
+     }
+    
+  }
+   let newArray = arrayString.map((item)=>{
+      return item + arrayAddition.join(`${options.additionSeparator}`); 
+   });
+   if(typeof options.separator != "string"){
+     return newArray.join('+');
+   } else if(options.separator == undefined){
+    return newArray.join('');
+   }else{
+     return newArray.join(`${options.separator.toString()}`);
+   }
+  }
+  
 };
   
