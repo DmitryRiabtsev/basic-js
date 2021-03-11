17eb2283c7a32baf9bf20f7ac5158631735a8245
Dmitry <dim4ik7558118@gmail.com>
2021-03-12 00:26:40 +0300

    feat: compete test

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,9 +1,18 @@
+const { restore } = require("sinon");
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(arr) { 
-   throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-    
-  }
+  calculateDepth(arr) {
+    if(arr instanceof Array){
+      return arr.reduce((max, item)=>{
+        return 1 + Math.max(max, this.calculateDepth(item));
+      });
+    }else{
+      return -1;
+    }
+  } 
 };
+
+// return 1 + (arr instanceof Array ? arr.reduce(function(max, item) {
+//   return Math.max(max, calculateDepth(item));
+// }, 0) : -1);
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  array: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.array.join('~~').length();
+    return this;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+     this.array.push(`( ${value} )`);
+     return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(typeof position != 'number' || !Number.isInteger(position) || position < 0){
+      this.array = [];
+      throw new CustomError('');
+    }else  {
+      this.array.splice(position - 1, 1);
+    }
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.array.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.array.join('~~');
+    this.array = [];
+    return result;
   }
 };
 
